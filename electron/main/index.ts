ee20230586ed6e64d46d732b93026867ad3549d2
jsxiaosi <1531733886@qq.com>
2024-12-22 16:57:30 +0800

    fix: 🐛 修复控制台不安全警告

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,6 +1,7 @@
+import process from 'node:process';
+import { fileURLToPath } from 'node:url';
 // 控制应用生命周期和创建原生浏览器窗口的模组
 import path from 'path';
-import { fileURLToPath } from 'node:url';
 import { app, BrowserWindow, ipcMain } from 'electron';
 
 globalThis.__filename = fileURLToPath(import.meta.url);
@@ -48,7 +49,7 @@ app.on('before-quit', () => {
   }
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
   // 通常在 macOS 上，当点击 dock 中的应用程序图标时，如果没有其他
   // 打开的窗口，那么程序会重新创建一个窗口。
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -56,6 +57,6 @@ app.on('activate', function () {
 
 // 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此，通常对程序和它们在
 // 任务栏上的图标来说，应当保持活跃状态，直到用户使用 Cmd + Q 退出。
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
